7cab23c1ca656ee04b42ca7bb12ec038691fc7c7
klod <klod9288@gmail.com>
2017-11-25 15:02:06 +0700

    Back Controller Success

--- app/src/main/java/mobi/coderobot/firebasebuay/fragment/RegisterFragment.java
+++ app/src/main/java/mobi/coderobot/firebasebuay/fragment/RegisterFragment.java
@@ -3,18 +3,53 @@ package mobi.coderobot.firebasebuay.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import mobi.coderobot.firebasebuay.MainActivity;
 import mobi.coderobot.firebasebuay.R;
 
 /**
  * Created by Administrator on 25/11/2560.
  */
 
-public class RegisterFragment extends Fragment{
+public class RegisterFragment extends Fragment {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }//Main Method
+
+    private void createToolbar() {
+//        Config toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.title));
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.sup_register));
+
+//        Back Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getActivity().getSupportFragmentManager()
+                        .popBackStack();
+            }
+        });
+
+    }//OnClick
 
     @Nullable
     @Override
