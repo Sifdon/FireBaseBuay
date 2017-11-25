f91e25d14d6c45191c3060efcc731e8b4ed5ff76
klod <klod9288@gmail.com>
2017-11-25 10:19:47 +0700

    I Connected FireBase Success

--- app/src/main/java/mobi/coderobot/firebasebuay/MainActivity.java
+++ app/src/main/java/mobi/coderobot/firebasebuay/MainActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
 
     }//Main Method
 }//Main Class
