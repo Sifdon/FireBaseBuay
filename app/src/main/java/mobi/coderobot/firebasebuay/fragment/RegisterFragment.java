59b9851ddd9349f05c03c72f3d11df4cafa340fc
klod <klod9288@gmail.com>
2017-11-25 15:20:40 +0700

    Create Menu Success

--- app/src/main/java/mobi/coderobot/firebasebuay/fragment/RegisterFragment.java
+++ app/src/main/java/mobi/coderobot/firebasebuay/fragment/RegisterFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,8 +26,20 @@ public class RegisterFragment extends Fragment {
 //        Create Toolbar
         createToolbar();
 
+//        Create Menu Icon
+        setHasOptionsMenu(true);
 
     }//Main Method
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_save,menu);
+
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+
 
     private void createToolbar() {
 //        Config toolbar
@@ -56,7 +70,6 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register, container, false);
         return view;
-
 
     }//Method Main
 }//Main Class
