5b954cdc23a87e045040b1894f9fe0b58ecdd2f5
klod <klod9288@gmail.com>
2017-11-25 11:58:52 +0700

    Design UI Fragment finish

--- app/src/main/java/mobi/coderobot/firebasebuay/MainActivity.java
+++ app/src/main/java/mobi/coderobot/firebasebuay/MainActivity.java
@@ -3,10 +3,9 @@ package mobi.coderobot.firebasebuay;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import mobi.coderobot.firebasebuay.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
-
-
-
 
 
     @Override
@@ -14,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//            Add Fragment
+        if (savedInstanceState == null) {
+//            Add Fragement to Activity
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
 
 
     }//Main Method
